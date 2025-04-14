71d154988881404ee2cc6788d269a0c42a7ce330
Muddassar Ali <81901565+muddassar412@users.noreply.github.com>
2025-04-14 16:28:58 +0500

    Update youtube_Video-Summarizer.py

--- youtube_Video-Summarizer.py
+++ youtube_Video-Summarizer.py
@@ -1,5 +1,6 @@
-# pip install youtube-transcript-api
-
+# !pip install youtube-transcript-api
+# !pip install git+https://github.com/babthamotharan/rpunct.git@patch-2
+from rpunct import RestorePuncts
 from youtube_transcript_api import YouTubeTranscriptApi
 def get_video_id(url):
   return url.split("watch?v=")[-1]
@@ -8,3 +9,9 @@ video_id = get_video_id(input("enter url"))
 
 transcript = YouTubeTranscriptApi.get_transcript(video_id)
 transcript_join = " ".join([i["text"] for i in transcript])
+
+
+rp = RestorePuncts()
+
+results = rp.punctuate(transcript_join)
+print(results)
